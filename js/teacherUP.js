4ba4d7588031ab0e1324a48af322d224d912e6a0
ziyovuddin <ziyovuddin.developer@gmail.com>
2023-05-29 19:15:55 +0500

    Baho qo'yish

--- js/teacherUP.js
+++ js/teacherUP.js
@@ -9,13 +9,13 @@ var send_s = localStorage.getItem('send_s');
 
 var container = document.getElementById('teacher-page');
 console.log(token);
-function errorMsg(){
+function errorMsg() {
     var tr = document.createElement('tr');
     tr.innerHTML = `<h1>Uzur xatolik</h1>`
     container.appendChild(div);
 }
 var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Token ${token}`);
+myHeaders.append("Authorization", `Token ${token}`);
 
 
 fetch(apiUrl + endpoint + send_s, {
@@ -27,22 +27,22 @@ fetch(apiUrl + endpoint + send_s, {
     .then(data => {
         console.log(data)
         data.status == "error" ? errorMsg() :
-                data.forEach(item => {
-                    var tr = document.createElement('tr');
-                    tr.innerHTML = `
+            data.forEach(item => {
+                var tr = document.createElement('tr');
+                tr.innerHTML = `
                     <td>${item.id}</td>
                     <td><a href="${item.file}">${item.file_name}</a></td>
                     
                     <td>${item.user}</td>
-                    <td> ${item.rating == false ? `<input type='number' class='input-group' id='ratingS' onchange='onchangeRat(${item.id})'>` :  item.rating}</td>
-                    <td> ${item.rating == false ? `<button  class='p-1 border-0' onclick='SendRat(${item.id})'>Jo\'natish</button>` :  item.rating}</td>
+                    <td> ${item.rating == false ? `<input type='number' class='input-group' id='ratingS' onchange='onchangeRat(${item.id})'>` : item.rating}</td>
+                    <td> ${item.rating == false ? `<button  class='p-1 border-0' onclick='SendRat(${item.id})'>Jo\'natish</button>` : ''}</td>
         `;
-                    container.appendChild(tr);
-                })
-    
+                container.appendChild(tr);
+            })
 
-      // Oynaga chiqarish
-      
+
+        // Oynaga chiqarish
+
     })
     .catch(error => {
         console.error('Xatolik yuz berdi:', error);
@@ -57,14 +57,48 @@ function fileSendT(params) {
     window.location.href = `${siteUrl}/student-file-upload.html`
     // confirm('jsjsjjs');
 }
+// let rating;
+// function onchangeRat(params){
+//     const ratingS = document.getElementById('ratingS').value;
+//     console.log(ratingS, params);
+//      rating = ratingS;
+//     return rating;
 
-function onchangeRat(params){
-    const ratingS = document.getElementById('ratingS').value;
-    console.log(ratingS, params);
-    return ratingS;
-    
-}
+// }
+// console.log(rating);
 // function SendRat(params) {
 
 //     alert(ratingS+" "+params);
 // }
+function onchangeRat(param) {
+    const ratingS = document.getElementById('ratingS').value;
+    console.log('Input qiymati:', param, ratingS);
+
+    // Boshqa funksiyada qiymatni ishlatish
+    anotherFunction(param, ratingS);
+}
+
+function anotherFunction(value, rat) {
+    console.log('Boshqa funksiya ichidagi qiymat:', value, rat);
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Token ${token}`);
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "task_id": value,
+        "rate": rat
+    });
+
+    var requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("https://eduhemisuz.pythonanywhere.com/send_work/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+        window.onload();
+}

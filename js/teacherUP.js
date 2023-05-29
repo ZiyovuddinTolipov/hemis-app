29fcf090435231986a37fce76423be86878c817f
ziyovuddin <ziyovuddin.developer@gmail.com>
2023-05-29 16:13:46 +0500

    auth

--- js/teacherUP.js
+++ js/teacherUP.js
@@ -1,53 +1,79 @@
-$(function () {
-    $("#wizard").steps({
-        headerTag: "h4",
-        bodyTag: "section",
-        transitionEffect: "fade",
-        enableAllSteps: true,
-        transitionEffectSpeed: 500,
-        onStepChanging: function (event, currentIndex, newIndex) {
-            if (newIndex === 1) {
-                $('.steps ul').addClass('step-2');
-            } else {
-                $('.steps ul').removeClass('step-2');
-            }
-            if (newIndex === 2) {
-                $('.steps ul').addClass('step-3');
-            } else {
-                $('.steps ul').removeClass('step-3');
-            }
 
-            if (newIndex === 3) {
-                $('.steps ul').addClass('step-4');
-                $('.actions ul').addClass('step-last');
-            } else {
-                $('.steps ul').removeClass('step-4');
-                $('.actions ul').removeClass('step-last');
-            }
-            return true;
-        },
-        labels: {
-            finish: "Order again",
-            next: "Next",
-            previous: "Previous"
-        }
-    });
-    // Custom Steps Jquery Steps
-    $('.wizard > .steps li a').click(function () {
-        $(this).parent().addClass('checked');
-        $(this).parent().prevAll().addClass('checked');
-        $(this).parent().nextAll().removeClass('checked');
-    });
-    // Custom Button Jquery Steps
-    $('.forward').click(function () {
-        $("#wizard").steps('next');
-    })
-    $('.backward').click(function () {
-        $("#wizard").steps('previous');
-    })
-    // Checkbox
-    $('.checkbox-circle label').click(function () {
-        $('.checkbox-circle label').removeClass('active');
-        $(this).addClass('active');
-    })
+var apiUrl = 'https://eduhemisuz.pythonanywhere.com';
+var endpoint = '/getwork/';
+var siteUrl = 'https://hemis-app.vercel.app';
+
+var token = localStorage.getItem('token');
+var send_s = localStorage.getItem('send_s');
+
+console.log(token);
+
+var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Token ${token}`);
+
+
+fetch(apiUrl + endpoint + send_s, {
+    method: 'GET',
+    headers: myHeaders,
+
 })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        var container = document.getElementById('teacher-page');
+        data.status == "error" ?
+            data.forEach(item => {
+                var tr = document.createElement('tr');
+                tr.innerHTML = `<h1>Uzur xatolik</h1>`
+                container.appendChild(div);
+            }) ?
+                data.forEach(item => {
+                    var tr = document.createElement('tr');
+                    tr.innerHTML = `
+        <i class="fa bg-blue fa-check" />
+        <div class="timeline-item">
+            <span class="time"><i class="fa fa-star" />
+
+
+            <h3 class="timeline-header ">  ${item.name}  <span style={{ color: "#fae8ff" }}>  
+            </span></h3>
+
+            <div class="timeline-body">
+                   <p>
+                ${item.description}
+                   <br />
+                </p>
+
+                <div class="timeline-footer mt30">
+                    <p class="text-mute">
+                        Topshiriq fayllari </p>
+                    <a class="download-item" href="${item.file}">
+                        <i class="fa fa-paperclip " />${item.file_name} <span class="pull-right">74 KiB</span>
+                    </a>
+                </div>
+                <p class="text-right">
+                <a class="btn btn-res" href="#"  onclick="fileSendS(${item.id})"> <i class="fa fa-reply"></i> Yuborganlar </a>
+                </p>
+            </div>
+        </span></div> 
+        `;
+                    container.appendChild(div);
+                })
+    
+}
+      // Oynaga chiqarish
+      
+    })
+    .catch(error => {
+        console.error('Xatolik yuz berdi:', error);
+    });
+function fileSendS(params) {
+    localStorage.setItem('send_s', params);
+    window.location.href = `${siteUrl}/student-file-upload.html`
+    // confirm('jsjsjjs');
+}
+function fileSendT(params) {
+    localStorage.setItem('send_s', params);
+    window.location.href = `${siteUrl}/student-file-upload.html`
+    // confirm('jsjsjjs');
+}

687530d1d3b001cb292f4cbbd4f4b40e9d650737
ziyovuddin <ziyovuddin.developer@gmail.com>
2023-05-31 02:30:54 +0500

    kutubxona

--- js/books.js
+++ js/books.js
@@ -1,31 +1,22 @@
 fetch('https://eduhemisuz.pythonanywhere.com/books/')
-.then( (response) => response.json()
-)
-.then((data) => {
-    // Process the returned data and generate HTML
-    var container = document.getElementById('ag-courses_box');
-    console.log(data);
-    var items = data.slice(1, 30)
-    items.forEach((item) => {
-        var div = document.createElement('div');
-        div.innerHTML = `
-       <div class="ag-courses_item col-4">
-    <a href="${item.file}" class="ag-courses-item_link">
-        <div class="ag-courses-item_bg"></div>
-
-        <div class="ag-courses-item_title">
-           ${item.name}
-        </div>
-
-        <div class="ag-courses-item_date-box">
-          
-        </div>
-    </a>
-</div>
+    .then((response) => response.json()
+    )
+    .then((data) => {
+        // Process the returned data and generate HTML
+        var container = document.getElementById('kutubxona');
+        console.log(data);
+        var items = data.slice(1, 30)
+        items.forEach((item) => {
+            var tr = document.createElement('tr');
+            tr.innerHTML = `
+                    <td>${item.id}</td>
+                    <td><a href="${item.file}" class="ag-courses-item_link">${item.file_name}</a></td>
+                    <td>${item.name}</td>
+                   
        `
-        container.appendChild(div);
+            container.appendChild(tr);
+        });
+    })
+    .catch(function (error) {
+        console.error('Error:', error);
     });
-})
-.catch(function (error) {
-    console.error('Error:', error);
-});
